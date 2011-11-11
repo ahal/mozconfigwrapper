e355ebcf5c1d2da0a29cb4c5f80197e4db03bbb8
Andrew Halberstadt <halbersa@gmail.com>
2011-11-11 18:22:19 -0500

    initial commit

---
+++ mozconfig.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+from optparse import OptionParser
+import shutil
+import sys
+import os
+
+"""
+Utility to make working with mozconfigs easier
+"""
+
+here = os.path.dirname(os.path.realpath(__file__))
+mozconfigdir = os.getenv('BUILDWITH_HOME')
+if mozconfigdir == None or mozconfigdir == '':
+     mozconfigdir = os.path.expanduser("~/.mozconfigs")
+
+def mkmozconfig(name):
+    if not os.path.isdir(mozconfigdir):
+        os.makedirs(mozconfigdir)
+
+    mozconfig = os.path.join(mozconfigdir, name)
+    template = os.path.join(mozconfigdir, 'template')
+    if not os.path.isfile(template):
+        shutil.copyfile(os.path.join(here, 'template'), template)
+    shutil.copyfile(template, mozconfig)
+
+    f = open(mozconfig, 'r')
+    lines = f.readlines()
+    f.close()
+    lines[0] = lines[0].rstrip() + name
+    f = open(mozconfig, 'w')
+    f.writelines(lines)
+    f.close()
+
+def mozconfig(arguments=sys.argv[1:]):
+    parser = OptionParser(description=__doc__, usage="%prog [options]")
+    parser.add_option("-l",
+                      dest="ls",
+                      action="store_true",
+                      default=False,
+                      help="lists all available mozconfigs")
+    parser.add_option('-e', '--edit',
+                      dest="edit",
+                      action="store_true",
+                      default=False,
+                      help="opens the mozconfig for editing"),
+    opt, args = parser.parse_args(arguments)
+
+    current = os.getenv('MOZCONFIG')
+    if not opt.ls and not opt.edit:
+        print current
+        return
+
+    if opt.ls:
+        for f in os.listdir(mozconfigdir):
+            if f != 'template':
+                if current not in [None, ''] and f == os.path.basename(current):
+                    f += "*"
+                print f
+
+    if opt.edit:
+        _edit(current)
+
+
+
+def _edit(mozconfig):
+    editor = os.getenv('EDITOR')
+    if not editor:
+        print "Can't open editor, EDITOR environment variable not set"
+    else:
+        cmd = editor + ' ' + mozconfig
+        os.system(cmd)
+
+
+if __name__ == '__main__':
+    sys.exit(mozconfig())
---
+++ mozconfigwrapper.sh
@@ -0,0 +1,80 @@
+# -*- mode: shell-script -*-
+
+function mozconfigwrapper_buildwith_home {
+    typeset buildwith_home=$BUILDWITH_HOME
+    if [ "$buildwith_home" = "" ]
+    then
+        buildwith_home="$HOME/.mozconfigs"
+        export BUILDWITH_HOME=$buildwith_home
+    fi
+}
+
+function buildwith {
+    mozconfigwrapper_buildwith_home
+    typeset name="$1"
+    
+    if [ ! -f "$BUILDWITH_HOME/$name" ]
+    then 
+        echo "Error: $BUILDWITH_HOME/$name does not exist"
+        return 1
+    fi
+
+    mozconfig="$BUILDWITH_HOME/$name"
+    export MOZCONFIG=$mozconfig
+    echo "$MOZCONFIG"
+    return 0
+}
+
+function mkmozconfig {
+    mozconfigwrapper_buildwith_home
+    typeset name="$1"
+
+    mozconfig="$BUILDWITH_HOME/$name"
+    python -c "from mozconfig import mkmozconfig; mkmozconfig('$name')"
+    echo "Created: $mozconfig"
+    buildwith $name
+}
+
+function rmmozconfig {
+    mozconfigwrapper_buildwith_home
+    typeset name="$1"
+
+    if [ ! -f "$BUILDWITH_HOME/$name" ]
+    then
+        echo "Error: $BUILDWITH_HOME/$name does not exist"
+        return 1
+    fi
+
+    mozconfig="$BUILDWITH_HOME/$name"
+    rm $mozconfig
+    echo "Removed: $mozconfig"
+}
+
+# Set up tab completion
+# Adapted from virtualenvwrapper (written by Doug Hellman and Arthur Koziel)
+function mozconfigwrapper_setup_tab_completion {
+    if [ -n "$BASH" ] ; then
+        _mozconfigs() {
+            local cur="${COMP_WORDS[COMP_CWORD]}"
+            COMPREPLY=( $(compgen -W "`mozconfigwrapper_list_mozconfigs`" -- ${cur}) )
+        }
+        complete -o default -o nospace -F _mozconfigs buildwith rmmozconfig
+    elif [ -n "$ZSH_VERSION" ] ; then
+        _virtualenvs () {
+            reply=( $(mozconfigwrapper_list_mozconfigs) )
+        }
+        compctl -K _virtualenvs buildwith rmmozconfig
+    fi
+}
+
+# List the available mozconfigs.
+function mozconfigwrapper_list_mozconfigs {
+    # NOTE: DO NOT use ls here because colorized versions spew control characters
+    #       into the output list.
+    # echo seems a little faster than find, even with -depth 3.
+    mozconfigwrapper_buildwith_home
+    (\cd "$BUILDWITH_HOME"; for f in `dir -d *`; do echo $f; done)
+}
+
+mozconfigwrapper_buildwith_home
+mozconfigwrapper_setup_tab_completion
